3afbfe9d7504e399ff64bbb1483e8786ab260773
Kerry Wang <kwzeyunwang@gmail.com>
2022-10-24 00:42:36 -0500

    shadow text effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,23 @@
   import Hex from "../components/hex.svelte";
 </script>
 
-<div class="h-screen bg-[url('/home-bg-artwork.png')]">
-  <main class="container flex flex-col items-center bg-neutral-300 opacity-50">
-    <h1 class="text-3xl">Welcome to SvelteKit</h1>
-    <p>
-      Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+<div class="h-screen bg-[url('/home-bg-artwork.png')] bg-cover bg-center">
+  <main
+    class="container flex h-screen flex-col items-center justify-center border border-white md:max-w-screen-md"
+  >
+    <div class="relative z-0 mb-10">
+      <h1 class="z-0 text-center text-4xl font-black uppercase md:text-7xl">
+        Happy Birthday <span class="text-selen">Selen</span>
+      </h1>
+      <h2
+        class="absolute top-1/2 left-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 text-center text-8xl font-black uppercase tracking-widest text-black opacity-30 md:text-9xl"
+      >
+        Selen Tatsuki
+      </h2>
+    </div>
+    <p class="z-10 font-serif">
+      A website designed by Selen Tatsuki’s dragoons celebrating her birthday!
     </p>
-    <a href="/about">About</a>
     <div class="flex flex-row flex-wrap">
       <a href="/about">
         <Hex />
