80f6ec2449d7513efd9c46ee589b4c6cd5d005b4
Kerry Wang <kwzeyunwang@gmail.com>
2022-10-26 16:24:46 -0500

    add obsydia logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,7 @@
   class="relative flex min-h-screen overflow-x-hidden border-t-4 border-t-selen bg-[url('/banner.png')] bg-cover bg-center text-white"
 >
   <slot />
+  <div
+    class="fixed bottom-1 right-1 h-32 w-32 bg-[url('/obsydia.webp')] bg-contain"
+  />
 </div>
