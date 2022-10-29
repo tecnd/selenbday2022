6c617e36110c8b00252bebf319a76a954d848e45
Kerry Wang <kwzeyunwang@gmail.com>
2022-10-28 23:26:47 -0500

    move back arrow to layout

--- src/routes/(subpages)/+layout.svelte
+++ src/routes/(subpages)/+layout.svelte
@@ -4,3 +4,12 @@
 
 <LoadingAnimation />
 <slot />
+<a href="/" class="fixed top-4 left-4"
+  ><i class="fa-solid fa-arrow-left fa-2x text-selen" /></a
+>
+
+<style>
+  i {
+    text-shadow: 0 0 4px black;
+  }
+</style>
--- src/routes/(subpages)/cakes/+page.svelte
+++ src/routes/(subpages)/cakes/+page.svelte
@@ -1,7 +1,4 @@
 <main class="container flex flex-col justify-center">
-  <a href="/" class="fixed top-4 left-4"
-    ><i class="fa-solid fa-arrow-left fa-2x text-selen" /></a
-  >
   <img
     class="max-h-full object-scale-down py-2"
     src="https://via.placeholder.com/1920x1080.gif"
@@ -12,8 +9,5 @@
 <style>
   main {
     height: calc(100vh - 4px);
-  }
-  i {
-    text-shadow: 0 0 4px black;
   }
 </style>
